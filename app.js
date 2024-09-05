a7f65825a1d33176c86627ef5b4b663e8486ea4c
Gabriel Faria <gabrielfaria88@gmail.com>
2024-09-05 17:29:42 -0300

    feat: implementing search feature

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-console.log(dados);
-
+let input = document.getElementById("input-search")
 const section = document.getElementById("resultados-pesquisa");
 
 let resultados = ''
 
-for (let dado of dados) {
+/* for (let dado of dados) {
     
     resultados += 
-        `
-            <div class="item-resultado">
-                <h2>${dado.titulo}</h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
-        `
-}
+    `
+    <div class="item-resultado">
+    <h2>${dado.titulo}</h2>
+    <p class="descricao-meta">${dado.descricao}</p>
+    <a href=${dado.link} target="_blank">Mais informações</a>
+    </div>
+    `
+} */
 
 section.innerHTML = resultados
+
+const search = () => {
+    let nomeMin = input.value.toLowerCase()
+    let filtrado = dados.filter((dado) => {
+     return dado.titulo.toLowerCase() === nomeMin
+    })
+    console.log(filtrado)
+    section.innerHTML = `
+    <div class="item-resultado">
+    <h2>${filtrado[0].titulo}</h2>
+    <p class="descricao-meta">${filtrado[0].descricao}</p>
+    <a href=${filtrado[0].link} target="_blank">Mais informações</a>
+    </div>
+    `
+}
