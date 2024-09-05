226178bb11e537e310d6aa9cfa74758d3a1942f9
Gabriel Faria <gabrielfaria88@gmail.com>
2024-09-05 16:09:14 -0300

    feat: dynamically adding infos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(dados)
+console.log(dados);
+
+const section = document.getElementById("resultados-pesquisa");
+
+let resultados = ''
+
+for (let dado of dados) {
+    
+    resultados += 
+        `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `
+}
+
+section.innerHTML = resultados
