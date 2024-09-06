93cbbc5d330d719969ca3f56141731b43b7bb7ab
Gabriel Faria <gabrielfaria88@gmail.com>
2024-09-06 17:50:59 -0300

    feat: adding if clauses into search

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-let input = document.getElementById("input-search")
+
 const section = document.getElementById("resultados-pesquisa");
 
-let resultados = ''
-
-/* for (let dado of dados) {
-    
-    resultados += 
-    `
-    <div class="item-resultado">
-    <h2>${dado.titulo}</h2>
-    <p class="descricao-meta">${dado.descricao}</p>
-    <a href=${dado.link} target="_blank">Mais informações</a>
-    </div>
-    `
-} */
-
-section.innerHTML = resultados
-
 const search = () => {
+    let resultados = ''
+    let input = document.getElementById("input-search")
+    input = input.value.toLowerCase()
+    if (!input) {
+        alert('Digite um termo no campo de pesquisa!')
+        return
+    }  
+    for (let dado of dados) {
+        if (dado.titulo.toLowerCase().includes(input) || dado.descricao.toLowerCase().includes(input) || dado.tags.toLowerCase().includes(input)) {
+        resultados += 
+        `
+            <div class="item-resultado">
+            <h2>${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+            `
+        } 
+        if (!resultados) {
+            resultados = `<p>Não houve resultado de pesquisa para o termo "<strong>${input}</strong>"</p>`
+        }
+    } 
+    section.innerHTML = resultados
+}
+
+
+/* const search = () => {
     let nomeMin = input.value.toLowerCase()
     let filtrado = dados.filter((dado) => {
      return dado.titulo.toLowerCase() === nomeMin
@@ -30,4 +41,4 @@ const search = () => {
     <a href=${filtrado[0].link} target="_blank">Mais informações</a>
     </div>
     `
-}
+} */
--- data.js
+++ data.js
@@ -3,15 +3,18 @@ let dados = [
         titulo: 'Tapestry',
         descricao: 'Tapestry é um jogo de tabuleiro estratégico que te convida a construir sua própria civilização, desde os primórdios até o futuro. Ao invés de competir diretamente com outros jogadores, você foca em desenvolver sua civilização ao longo de quatro trilhas principais: ciência, tecnologia, exploração e militar.',
         link: 'https://ludopedia.com.br/jogo/tapestry',
+        tags: 'dados avanço tapeçaria recursos'
     }, 
     {
         titulo: 'Gloomhaven: Jaws of the Lion',
         descricao: 'Gloomhaven: Jaws of the Lion é um jogo de aventura tática para um a quatro jogadores, que serve como uma introdução ao universo de Gloomhaven. Nele, você controla um grupo de aventureiros que exploram um mundo em ruínas, enfrentando perigos e desvendando mistérios.',
         link: 'https://ludopedia.com.br/jogo/gloomhaven-jaws-of-the-lion',
+        tags: 'dungeon crawler campanha capítulo'
     }, 
     {
         titulo: 'The Castles of Burgundy',
-        descricao: 'The Castles of Burgundy é um jogo de colocação de trabalhadores em que os jogadores assumem o papel de nobres que constroem seus próprios castelos na região da Borgonha. O objetivo é acumular a maior quantidade de pontos através da construção de castelos, cidades e vinhedos.',
+        descricao: 'The Castles of Burgundy é um jogo de colocação de trabalhadores em que os jogadores assumem o papel de nobres que constroem seus próprios castelos na região da Borgonha. O objetivo é acumular a maior quantidade de pontos através da construção de castelos, cidades e fazendas.',
         link: 'https://ludopedia.com.br/jogo/the-castles-of-burgundy',
+        tags: 'barco mercadoria minas ciência dados'
     }, 
 ]
